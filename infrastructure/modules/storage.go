fabe926626846d195d5615cf247c5fc2b88f48a9
iwaseshi <aaa.1470.333@gmail.com>
2024-04-06 16:03:15 +0900

    feat: storagebucketiampolicy

--- infrastructure/modules/storage.go
+++ infrastructure/modules/storage.go
@@ -3,12 +3,28 @@ package modules
 import (
 	"github.com/aws/jsii-runtime-go"
 	"github.com/cdktf/cdktf-provider-google-go/google/v6/storagebucket"
+	"github.com/cdktf/cdktf-provider-google-go/google/v6/storagebucketiampolicy"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
 func NewStorageBucket(stack cdktf.TerraformStack) {
-	storagebucket.NewStorageBucket(stack, jsii.String("gcs_bucket"), &storagebucket.StorageBucketConfig{
+	storage := storagebucket.NewStorageBucket(stack, jsii.String("gcs_bucket"), &storagebucket.StorageBucketConfig{
 		Location: jsii.String("asia-northeast1"),
 		Name:     jsii.String("gunpla-calendar-exporter"),
 	})
+
+	storagebucketiampolicy.NewStorageBucketIamPolicy(stack, jsii.String("bucket_iam"), &storagebucketiampolicy.StorageBucketIamPolicyConfig{
+		Bucket: storage.Name(),
+		PolicyData: jsii.String(`{
+			"bindings": [
+				{
+					"role":"roles/storage.legacyObjectReader",
+					"members": [
+						"allUsers"
+					]
+				}
+			]
+		}`),
+	})
+
 }
