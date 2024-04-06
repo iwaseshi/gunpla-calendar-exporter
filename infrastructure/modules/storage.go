efd29d45b8f1baddf8fd26fb6da618b678d9efa2
iwaseshi <aaa.1470.333@gmail.com>
2024-04-06 21:16:09 +0900

    refactor: storage policy

--- infrastructure/modules/storage.go
+++ infrastructure/modules/storage.go
@@ -2,14 +2,41 @@ package modules
 
 import (
 	"github.com/aws/jsii-runtime-go"
+	"github.com/cdktf/cdktf-provider-google-go/google/v6/serviceaccount"
 	"github.com/cdktf/cdktf-provider-google-go/google/v6/storagebucket"
+	"github.com/cdktf/cdktf-provider-google-go/google/v6/storagebucketiampolicy"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
 func NewStorageBucket(stack cdktf.TerraformStack) {
-	storagebucket.NewStorageBucket(stack, jsii.String("gcs_bucket"), &storagebucket.StorageBucketConfig{
+	bucket := storagebucket.NewStorageBucket(stack, jsii.String("gcs_bucket"), &storagebucket.StorageBucketConfig{
 		Location: jsii.String("asia-northeast1"),
 		Name:     jsii.String("gunpla-calendar-exporter"),
+	})
+
+	account := serviceaccount.NewServiceAccount(stack, jsii.String("app_sa"), &serviceaccount.ServiceAccountConfig{
+		AccountId:   jsii.String("app-account"),
+		DisplayName: jsii.String("app account"),
+	})
+
+	storagebucketiampolicy.NewStorageBucketIamPolicy(stack, jsii.String("sa_iam"), &storagebucketiampolicy.StorageBucketIamPolicyConfig{
+		Bucket: bucket.Name(),
+		PolicyData: jsii.String(`{
+			"bindings": [
+				{
+					"role": "roles/storage.objectAdmin",
+					"members": [
+						"serviceAccount:` + *account.Email() + `"
+					]
+				},
+				{
+					"role":"roles/storage.legacyObjectReader",
+					"members": [
+						"allUsers"
+					]
+				}
+			]
+		}`),
 	})
 
 }
