71421304909448a3038faf7cd184386a86c9b31a
iwaseshi <aaa.1470.333@gmail.com>
2024-04-23 21:19:09 +0900

    refactor

--- internal/upload/storage.go
+++ internal/upload/storage.go
@@ -11,9 +11,7 @@ import (
 	"google.golang.org/api/option"
 )
 
-func CloudStorage(targetFilePath string) error {
-
-	ctx := context.Background()
+func CloudStorage(ctx context.Context, targetFilePath string) error {
 
 	file, err := os.Open(targetFilePath)
 	if err != nil {
@@ -22,29 +20,45 @@ func CloudStorage(targetFilePath string) error {
 	defer file.Close()
 
 	bucketName := "gunpla-calendar-exporter"
-	credentialsFile := "storage_key.json"
+	now := time.Now()
+	objectPath := fmt.Sprintf("%d%s.ics", now.Year(), now.AddDate(0, 0, 20).Month().String())
 
-	client, err := storage.NewClient(ctx, option.WithCredentialsFile(credentialsFile))
+	storage, err := newCloudStorage(ctx, bucketName)
 	if err != nil {
 		return err
 	}
+	if err = storage.WriteObject(file, objectPath); err != nil {
+		return err
+	}
+	fmt.Println("file upload success:" + fmt.Sprintf("https://storage.cloud.google.com/%s/%s", bucketName, objectPath))
+	return nil
+}
 
-	now := time.Now()
-	objectPath := fmt.Sprintf("%d%s.ics", now.Year(), now.Month().String())
-	obj := client.Bucket(bucketName).Object(objectPath)
+type cloudStorage struct {
+	bucket string
+	client *storage.Client
+	ctx    context.Context
+}
 
-	wc := obj.NewWriter(ctx)
+func newCloudStorage(ctx context.Context, bucket string) (*cloudStorage, error) {
+	client, err := storage.NewClient(ctx, option.WithCredentialsFile("storage_key.json"))
+	if err != nil {
+		return nil, err
+	}
+	return &cloudStorage{
+		bucket: bucket,
+		client: client,
+		ctx:    ctx,
+	}, nil
+}
+
+func (st cloudStorage) WriteObject(file *os.File, objectPath string) error {
+	obj := st.client.Bucket(st.bucket).Object(objectPath)
+	wc := obj.NewWriter(st.ctx)
 	wc.ContentType = "text/calendar"
 	if _, err := io.Copy(wc, file); err != nil {
 		return err
 	}
 	defer wc.Close()
-
-	acl := client.Bucket(bucketName).Object(objectPath).ACL()
-	if err := acl.Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
-		return err
-	}
-
-	fmt.Println("file upload success:" + fmt.Sprintf("https://storage.cloud.google.com/%s/%s", bucketName, objectPath))
 	return nil
 }
