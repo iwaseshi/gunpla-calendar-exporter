d3ec30c8ff6931ada8921649be251fa673b736b1
iwaseshi <aaa.1470.333@gmail.com>
2024-05-09 23:37:57 +0900

    feat: update dep

--- infrastructure/modules/storage.go
+++ infrastructure/modules/storage.go
@@ -2,9 +2,9 @@ package modules
 
 import (
 	"github.com/aws/jsii-runtime-go"
-	"github.com/cdktf/cdktf-provider-google-go/google/v6/serviceaccount"
-	"github.com/cdktf/cdktf-provider-google-go/google/v6/storagebucket"
-	"github.com/cdktf/cdktf-provider-google-go/google/v6/storagebucketiampolicy"
+	"github.com/cdktf/cdktf-provider-google-go/google/v13/serviceaccount"
+	"github.com/cdktf/cdktf-provider-google-go/google/v13/storagebucket"
+	"github.com/cdktf/cdktf-provider-google-go/google/v13/storagebucketiampolicy"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
