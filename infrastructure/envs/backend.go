601924922eff7628c2eb2211deb8680511cfdef7
iwaseshi <aaa.1470.333@gmail.com>
2024-05-21 04:41:46 +0900

    chore

--- infrastructure/envs/backend.go
+++ infrastructure/envs/backend.go
@@ -9,6 +9,5 @@ func SetupGcsBackend(stack cdktf.TerraformStack) {
 	cdktf.NewGcsBackend(stack, &cdktf.GcsBackendConfig{
 		Bucket: jsii.String("gunpla-calendar-exporter-backend"),
 		Prefix: jsii.String("terraform/state"),
-		// 個人開発のためstate lockは不要
 	})
 }
